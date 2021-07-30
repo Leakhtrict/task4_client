59732bb7bd935f1ea0457d95986cc8e0974b603d
agenis <artyomgenis@gmail.com>
2021-07-30 12:45:06 +0300

    minor changes in front-end #8.2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -161,6 +161,21 @@ function Home() {
             })}
           </tbody>
         </table>
+        <p>Selected Rows: {Object.keys(selectedRowIds).length}</p>
+      <pre>
+        <code>
+          {JSON.stringify(
+            {
+              selectedRowIds: selectedRowIds,
+              'selectedFlatRows[].original': selectedFlatRows.map(
+                d => d.original
+              ),
+            },
+            null,
+            2
+          )}
+        </code>
+      </pre>
       </>
     );
   };
